2a6bce282c1defd60f55839b90562c2ebd9488a2
unknown <dimitamp@auth.gr>
2019-04-12 22:38:05 +0300

    Updates

---
+++ precache-manifest.273ec5a375cb0c3ded969523e41f25aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a51267c4e6d4c0467566fda356fca620",
+    "url": "/portfolio/static/media/profile_photo.a51267c4.jpg"
+  },
+  {
+    "revision": "46509adeb4e907f362ba1a319dbc99bf",
+    "url": "/portfolio/static/media/christmashat.46509ade.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "31f20be220af1d14dc97",
+    "url": "/portfolio/static/js/main.31f20be2.chunk.js"
+  },
+  {
+    "revision": "c133b81000261c20ec0e",
+    "url": "/portfolio/static/js/1.c133b810.chunk.js"
+  },
+  {
+    "revision": "31f20be220af1d14dc97",
+    "url": "/portfolio/static/css/main.297f8008.chunk.css"
+  },
+  {
+    "revision": "f79a1189ce398ea12b1101abf3de13b1",
+    "url": "/portfolio/index.html"
+  }
+];
